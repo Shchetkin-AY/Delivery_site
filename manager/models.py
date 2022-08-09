f20a2e042012d031bb6251e65edc26eaf2455b90
Leman190 <shchetkin.a.y@gmail.com>
2022-08-09 17:34:49 +0700

    Added main_page and nav_bar

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# class Agent(models.Model):
+#     company = models.CharField(max_length=20)
+#     phone = models.CharField(max_length=11)
+#     inn_number = models.IntegerField(max_length=10)
+#
+# class PackingList(models.Model):
+#     name = models.CharField(max_length=50)
+#     weight = models.IntegerField(max_length=5)
+#     volume = models.IntegerField(max_length=3)
+#     places_count = models.IntegerField(max_length=3)
+
+
