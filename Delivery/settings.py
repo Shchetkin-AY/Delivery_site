7adb278019df8bd9ea9d8209ab7d62a96847e40d
Alexandr <shchetkin.a.y@gmail.com>
2022-08-17 00:07:36 +0700

    Added calculator prise and some visual fix

--- Delivery/settings.py
+++ Delivery/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wkhtmltopdf',
     'crispy_forms',
     'manager'
 ]
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'Delivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_delivery',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
@@ -118,8 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+USE_L10N = False
+DATE_INPUT_FORMATS = ['%d/%m/%Y']
+
+STATIC_URL = 'manager/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'manager/static'),
+]
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- manager/forms.py
+++ manager/forms.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 
-from django.forms import forms, ModelForm
+from django.forms import  ModelForm
 
 from manager.models import PackingList, Agent
 
@@ -11,22 +11,18 @@ class PackingListForm(ModelForm):
         labels = {'sender': _('Отправитель'),
                   'destination': _('Получатель'),
                   'content': _('Описание груза'),
-                  'weight': _('Вес'),
-                  'volume': _('Объем'),
-                  'places_count': _('Количество мест'),
-                  'price': _('Стоимость')
+                  'weight': _('Вес, кг'),
+                  'volume': _('Объем, м.куб'),
+                  'places_count': _('Количество мест, шт.'),
+                  'price': _('Стоимость, руб.')
                   }
 
 class AgentForm(ModelForm):
     class Meta:
         model = Agent
-        fields = '__all__'
+        fields = []
         labels = {'company': _('Название'),
                   'phone': _('Номер телефона'),
                   'inn_number': _('ИНН'),
                   }
 
-        # error_messages = {'company': { 'value is less': ('Неверный формат названия компании')},
-                         # 'phone': ('Неверный формат номера телефона'),
-                         # 'inn_number': {'max_length': ('ИНН должен состоять из 10 цифр')}
-                         #               }
