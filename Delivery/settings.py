302cc032855a76c186510235080e003287c1438e
Leman190 <shchetkin.a.y@gmail.com>
2022-08-22 12:06:50 +0700

    Fourth attempt fixed problem with db on deploy

--- Delivery/settings.py
+++ Delivery/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'db_delivery',
         'USER': 'postgres',
         'PASSWORD': 'admin',
-        'HOST': '127.0.0.1',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
