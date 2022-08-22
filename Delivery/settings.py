f5fd3a582bce89e600e324ea8a79497d2f2c94db
Leman190 <shchetkin.a.y@gmail.com>
2022-08-22 11:00:30 +0700

    First attempt fixed problem with db on deploy

--- Delivery/settings.py
+++ Delivery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,3 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGOUT_REDIRECT_URL = '/'
+
+django_heroku.settings(locals())
