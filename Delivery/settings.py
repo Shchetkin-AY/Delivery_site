db2c731d1cfcd0328a8a877cc86b7e0d122c9aa4
Alexandr <shchetkin.a.y@gmail.com>
2022-09-10 21:45:10 +0700

    Debug False

--- Delivery/settings.py
+++ Delivery/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ol*(ieplca)egpkhw!bw@1lesqz@u*#0tnf@i^s81$(7%&(b%g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '*', 'localhost', '127.0.0.1', 'manager-delivery.herokuapp.com']
 
